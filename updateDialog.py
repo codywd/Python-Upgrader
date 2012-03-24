5702787feb26c13662d3d04cf733bc74ebee2b4d
Cody Dostal <codydostal@gmail.com>
2012-03-24 00:18:23 -0600

    Any updates before moving to Fedora

--- updateDialog.py
+++ updateDialog.py
@@ -8,7 +8,7 @@ class updateDialog():
     try:
         #Example upFile: https://raw.github.com/Seafire-Software/Seapad/master/curVersion
         upFile = "SET-YOUR-CURRENT-VERSION-FILE-HERE"
-        #Example UpGrade File: https://raw.github.com/Seafire-Software/Seapad/master/SeaPad.py
+        #Example upgradeFile: https://raw.github.com/Seafire-Software/Seapad/master/SeaPad.py
         upgradeFile = "SET-YOUR-FILE-UPGRADING-WEB-PATH-HERE"
         
         
@@ -17,7 +17,7 @@ class updateDialog():
         basVer = open(tempDir + '/curVersion')
         ver = float(basVer.read())
         
-        # Set a your current Program Version as a float (ex: progVer = 1.00)
+        # Set your current Program Version as a float (ex: progVer = 1.00)
         if progVer < ver:
             dlg = wx.MessageDialog(None, "You must update. Your version is " + str(progVer) + ", and the latest version is " + str(ver) + ", Do you wish to update?", "Update Required.", wx.YES_NO | wx.ICON_INFORMATION)
            
@@ -25,7 +25,7 @@ class updateDialog():
             dlg.Destroy()
             if result == wx.ID_YES:    
                 urllib.urlretrieve(upgradeFile, os.path.join(sys.path[0], sys.argv[0]))
-                dlg = wx.MessageDialog(None, "Update successful. Please restart the program!", "Restart manually.", wx.YES_NO)
+                dlg = wx.MessageBox("Update successful. Please restart the program!", "Restart manually.", wx.OK)
             else:
                 wx.MessageBox("You chose not to upgrade. Please upgrade later!", "Upgrade later.", wx.OK)
         elif ver == progVer:
