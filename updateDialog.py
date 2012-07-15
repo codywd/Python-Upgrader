a584ea98acedb865141a6f026b59609c0a7c68fb
Cody Dostal <dostalcody@gmail.com>
2012-07-15 17:29:11 -0600

    Update. Newer way of doing upgrading

--- updateDialog.py
+++ updateDialog.py
@@ -9,27 +9,31 @@ import wx
 
 
 class updateDialog():
+    
     try:
         #Example upFile: https://raw.github.com/Seafire-Software/Seapad/master/curVersion
-        upFile = "SET-YOUR-CURRENT-VERSION-FILE-HERE"
-        #Example upgradeFile: https://raw.github.com/Seafire-Software/Seapad/master/SeaPad.py
-        upgradeFile = "SET-YOUR-FILE-UPGRADING-WEB-PATH-HERE"
-        
-        # tempfile.gettempdir() is a cross-platform way to find the temporary directory of the current OS
+        upFile = "SET-YOUR-CURVERSION-HERE"
         tempDir = tempfile.gettempdir()
         webFile = urllib.urlretrieve(upFile, tempDir + '/curVersion')
         basVer = open(tempDir + '/curVersion')
         ver = float(basVer.read())
-        
-        # Set your current Program Version as a float (ex: progVer = 1.00)
+                
         if progVer < ver:
+            #You have to set a variable as progVer somewhere.
             dlg = wx.MessageDialog(None, "You must update. Your version is " + str(progVer) + ", and the latest version is " + str(ver) + ", Do you wish to update?", "Update Required.", wx.YES_NO | wx.ICON_INFORMATION)
-           
+                   
             result = dlg.ShowModal()
             dlg.Destroy()
-            if result == wx.ID_YES:    
-                urllib.urlretrieve(upgradeFile, os.path.join(sys.path[0], sys.argv[0]))
-                dlg = wx.MessageBox("Update successful. Please restart the program!", "Restart manually.", wx.OK)
+            if result == wx.ID_YES:
+                if sys.platform == "linux2":
+                    #Example upgradeFile: https://raw.github.com/Seafire-Software/Seapad/master/SeaPad.py
+                    upgradeFile = "SET-YOUR-UPGRADE-FILE-HERE"
+                elif sys.platform == "win32":
+                    #Example upgradeFile: https://raw.github.com/Seafire-Software/Seapad/master/SeaPad.exe
+                    upgradeFile = "https://github.com/Seafire-Software/Anansi-CalcPad/blob/master/Binary/Anansi-CalcPad-Setup.exe?raw=true"
+                   
+                urllib.urlretrieve(upgradeFile, os.path.join(os.getcwd(), sys.argv[0]))
+                dlg = wx.MessageDialog(None, "Update successful. Please restart the program!", "Restart manually.", wx.YES_NO)
             else:
                 wx.MessageBox("You chose not to upgrade. Please upgrade later!", "Upgrade later.", wx.OK)
         elif ver == progVer:
@@ -38,6 +42,5 @@ class updateDialog():
             wx.MessageBox("What happened here? Your version is " + str(progVer) + " which is greater than the latest version of " + str(ver), "Your version is greater than ours?", wx.OK | wx.ICON_QUESTION)
         else:
             wx.MessageBox("Something went wrong with the update. Please try again. If it happens again, file a bug report please.", "Error!", wx.OK | wx.ICON_ERROR)            
-    except:
-        ## The Error number (10152) is optional. You can remove if you don't add it on your site somewhere. ##
-        wx.MessageBox("There was an error (Error 10152). Maybe you are not connected to the internet? Try again please.", "Try again.", wx.OK)
+        except:
+            wx.MessageBox("There was an error (Error 10152). Maybe you are not connected to the internet? Try again please.", "Try again.", wx.OK)
